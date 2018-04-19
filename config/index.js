180566970ce60388abe421148406cae7c9aa8180
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2018-04-19 15:02:39 +0200

    wee fixed the game //fabian

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+		assetsPublicPath: process.env.WEBPACK_PUBLIC_PATH || '/',
 
     /**
      * Source Maps
