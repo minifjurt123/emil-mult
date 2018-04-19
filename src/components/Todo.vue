2fdb51095bf3e563c7411712321389b0b5a8b084
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2018-04-19 19:26:00 +0200

    add todo & 404 handling

---
+++ src/components/Todo.vue
@@ -0,0 +1,17 @@
+<template>
+	<ul>
+		<li>"Last submitted" scoreboard</li>
+		<li>Ability to go to a bigger version of scoreboard (/scoreboard?) (endless scroll)</li>
+		<li>Bigger elements on game-UI</li>
+	</ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,39 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Game from '@/components/Game'
 import Home from '@/components/Home'
+import ScoreBoard from '@/components/ScoreBoard'
+import Todo from '@/components/Todo'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home,
-      props: true
-    },
-    {
-      path: '/play',
-      name: 'Game',
-      component: Game
-    },
-  ]
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: Home,
+			props: true
+		},
+		{
+			path: '/play',
+			name: 'Game',
+			component: Game
+		},
+		{
+			path: '/scoreboard',
+			name: 'Scoreboard',
+			component: ScoreBoard
+		},
+		{
+			path: '/todo',
+			name: 'Todo',
+			component: Todo
+		},
+		// Soft 404 handling
+		{
+			path: '*',
+			redirect: { name: 'Home' },
+		}
+	]
 })
