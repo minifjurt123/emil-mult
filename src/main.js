041c74d9f1054ff69a58884f25c87fc705e3fcb7
Emil Ahlbäck <Emil.Ahlback99@gmail.com>
2018-04-19 14:23:36 +0200

    changed api

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 Vue.config.productionTip = false
 
 Vue.prototype.$http = axios.create({
-  baseURL: 'http://localhost:1234/'
+  baseURL: 'http://100.benefitcraft.me/mult-api'
 })
 
 /* eslint-disable no-new */
